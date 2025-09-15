69fb23f81defe6b353d35d18da784f907551445c
Arun Lal <118952274+arunlalp@users.noreply.github.com>
2025-09-15 15:02:53 +0530

    Update background.sh

--- container-course/lab-02/scripts/background.sh
+++ container-course/lab-02/scripts/background.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
+exec >>/tmp/setup.log 2>&1
 
-# show echo messages on terminal
-echo "Starting background setup..."
+# simulate a long setup
+echo "Starting background setup..." >/dev/tty
+sleep 25
 
-# redirect rest of logs to file
-{
-  # simulate a long setup
-  sleep 25
-
-  # e.g. apt-get update && apt-get install -y nginx
-  touch /tmp/.scenario_ready
-} >>/tmp/setup.log 2>&1
+# e.g. apt-get update && apt-get install -y nginx
+touch /tmp/.scenario_ready
