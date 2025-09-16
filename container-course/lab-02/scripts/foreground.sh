3f9b76c86c85811f2257863472c2e325d0676dfc
Arun Lal <118952274+arunlalp@users.noreply.github.com>
2025-09-16 14:56:03 +0530

    Revise Nginx lab step and scripts
    Expanded step1.md with detailed Nginx installation and process explanation. Updated step title in index.json. Simplified foreground.sh to a loading message and removed validate.sh script.

--- container-course/lab-02/scripts/foreground.sh
+++ container-course/lab-02/scripts/foreground.sh
@@ -1,17 +1,1 @@
-#!/bin/bash
-
-echo "Waiting for nginx to start..." >/dev/tty
-for i in {1..180}; do
-    if systemctl is-active --quiet nginx; then
-        break
-    fi
-    sleep 1
-done
-
-# Check if nginx is running
-if systemctl is-active --quiet nginx; then
-    echo "Nginx is running."
-else
-    echo "Nginx is not running after waiting."
-    exit 1
-fi
+echo "Scenario is loading..."
